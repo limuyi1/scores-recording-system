3505199d352db7883a92974e0fbfb9c0c26c9e66
李木一 <limuyiemail@163.com>
2024-06-21 23:27:01 +0800

    提交代码： 1.完成替换

--- src/untils/pdfUntil.ts
+++ src/untils/pdfUntil.ts
@@ -1,6 +1,6 @@
-import html2canvas from 'html2canvas'
 import domtoimage from 'dom-to-image'
 import { jsPDF } from 'jspdf'
+import { ElMessage } from 'element-plus'
 
 import { PagesEnum } from '@/types/Common'
 import { ElLoading } from 'element-plus'
@@ -15,7 +15,7 @@ import { ElLoading } from 'element-plus'
 const exportPDF = async (
   refs: any,
   pageType: PagesEnum = PagesEnum.A4,
-  scale: number = 3,
+  scale: number = 4,
   fileName: string = new Date().toLocaleString() + '.pdf'
 ) => {
   const loading = ElLoading.service({
@@ -23,34 +23,51 @@ const exportPDF = async (
     text: '正在导出PDF...',
     background: 'rgba(0, 0, 0, 0.7)'
   })
-  const doc = new jsPDF({
-    orientation: 'portrait',
-    unit: 'px',
-    format: pageType,
-    compress: true, // 压缩文档
-    precision: 16 // 浮点数的精度
-  })
 
-  for (const [index, ref] of Array.from(refs).entries()) {
-    const imageUrl = await domtoimage.toPng(ref, {
-      bgcolor: '#FFFFFF'
+  try {
+    const doc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'px',
+      format: pageType,
+      compress: true, // 压缩文档
+      precision: 16 // 浮点数的精度
     })
-    const imgData = new Image()
-    imgData.src = imageUrl
 
-    const imgProps = doc.getImageProperties(imgData)
-    const pdfWidth = doc.internal.pageSize.getWidth()
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width
-    doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight)
+    for (const [index, ref] of Array.from(refs).entries()) {
+      const elm: HTMLElement = ref as HTMLElement
 
-    if (index !== refs.length - 1) {
-      doc.addPage()
+      const imageUrl = await domtoimage.toJpeg(elm, {
+        quality: 1,
+        width: elm?.offsetWidth * scale,
+        height: elm?.offsetHeight * scale,
+        bgcolor: '#FFFFFF',
+        style: {
+          transform: `scale(${scale})`, // 放大元素倍数，提高清晰度
+          transformOrigin: '0 0' // 指定变换的原点
+        }
+      })
+      const imgData = new Image()
+      imgData.src = imageUrl
+
+      const imgProps = doc.getImageProperties(imgData)
+      const pdfWidth = doc.internal.pageSize.getWidth()
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width
+      doc.addImage(imgData, 'JPEG', 0, 0, pdfWidth, pdfHeight)
+
+      if (index !== refs.length - 1) {
+        doc.addPage()
+      }
     }
+
+    doc.save(fileName)
+
+    loading.close()
+  } catch (err) {
+    console.error(err)
+
+    ElMessage.error('导出失败！')
+    loading.close()
   }
-
-  doc.save(fileName)
-
-  loading.close()
 }
 
 export { exportPDF }
